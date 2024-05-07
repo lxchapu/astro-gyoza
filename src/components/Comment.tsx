f5d4b9e0c9981e0236096cac800411e297561d43
lxchapu <lxchapu@outlook.com>
2024-05-07 21:46:30 +0800

    fix: check if node exist when waline call cleanup

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -2,9 +2,9 @@ import { useEffect, useRef } from 'react'
 import { init } from '@waline/client'
 import { waline } from '@/config.json'
 import '@waline/client/style'
-
+// TODO: Use custom comment system
 export function Comment() {
-  const ref = useRef(null)
+  const ref = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     const walineInst = init({
@@ -13,13 +13,18 @@ export function Comment() {
       dark: "[data-theme='dark']",
       login: 'force',
       imageUploader: false,
+      search: false,
       locale: {
         placeholder: '发条友善的评论吧（支持 Markdown 语法）…',
       },
       emoji: ['//unpkg.com/@waline/emojis@1.1.0/bilibili'],
     })
 
-    return () => walineInst?.destroy()
+    return () => {
+      if (ref.current) {
+        walineInst?.destroy()
+      }
+    }
   }, [])
 
   return <div ref={ref}></div>
