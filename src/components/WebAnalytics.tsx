00d2e1494314fc4ae462dc6ec99762c51fce4626
lxchapu <lxchapu@outlook.com>
2024-05-21 20:26:40 +0800

    feat: inject Google and Umami tracking code

---
+++ src/components/WebAnalytics.tsx
@@ -0,0 +1,46 @@
+import { analytics } from '@/config.json'
+
+export function WebAnalytics() {
+  if (import.meta.env.DEV || !analytics.enbale) return null
+
+  return <>
+    {
+      analytics.umami.websitId && <UmamiAnalytics {...analytics.umami} />
+    }
+    {
+      analytics.google.measurementId && <GoogleAnalytics {...analytics.google} />
+    }
+  </>
+}
+
+function UmamiAnalytics({
+  serverUrl,
+  websitId,
+}: {
+  serverUrl?: string,
+  websitId: string,
+}) {
+  const src = `${serverUrl || 'https://cloud.umami.is'}/script.js`
+
+  return <script defer src={src} data-website-id={websitId} />
+}
+
+
+function GoogleAnalytics({
+  measurementId,
+}: {
+  measurementId: string,
+}) {
+  return (
+    <>
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}></script>
+      <script dangerouslySetInnerHTML={{
+        __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', '${measurementId}');`,
+      }}></script>
+    </>
+  )
+}
