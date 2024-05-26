30d0587e3ba403849608bd5836aebc552db5d1de
lxchapu <lxchapu@outlook.com>
2024-05-26 16:25:38 +0800

    refactor: re-export components in head

---
+++ src/components/head/index.ts
@@ -0,0 +1,5 @@
+export { default as AccentColorInjector } from './AccentColorInjector.astro'
+export { default as CommonHead } from './CommonHead.astro'
+export { default as PrintVersion } from './PrintVersion.astro'
+export { default as ThemeLoader } from './ThemeLoader.astro'
+export { WebAnalytics } from './WebAnalytics'
