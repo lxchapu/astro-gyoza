76188391090da8100d827c7d84fe37ee1bbc3935
Cedar <FYLSen@users.noreply.github.com>
2024-05-28 13:10:23 +0800

    feat: add Microsoft Clarity integration and correct typo in websiteId variable
    - Added Microsoft Clarity analytics integration in the WebAnalytics
    component.
    - Fixed a typo in the UmamiAnalytics component: changed 'websitId'
    variable to 'websiteId'.

--- src/components/head/WebAnalytics.tsx
+++ src/components/head/WebAnalytics.tsx
@@ -1,46 +1,68 @@
-import { analytics } from '@/config.json'
+  import { analytics } from '@/config.json'
 
-export function WebAnalytics() {
-  if (import.meta.env.DEV || !analytics.enable) return null
+  export function WebAnalytics() {
+    if (import.meta.env.DEV || !analytics.enable) return null
 
-  return <>
-    {
-      analytics.umami.websitId && <UmamiAnalytics {...analytics.umami} />
-    }
-    {
-      analytics.google.measurementId && <GoogleAnalytics {...analytics.google} />
-    }
-  </>
-}
-
-function UmamiAnalytics({
-  serverUrl,
-  websitId,
-}: {
-  serverUrl?: string,
-  websitId: string,
-}) {
-  const src = `${serverUrl || 'https://cloud.umami.is'}/script.js`
-
-  return <script defer src={src} data-website-id={websitId} />
-}
-
-
-function GoogleAnalytics({
-  measurementId,
-}: {
-  measurementId: string,
-}) {
-  return (
-    <>
-      <script async src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}></script>
-      <script dangerouslySetInnerHTML={{
-        __html: `window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', '${measurementId}');`,
-      }}></script>
+    return <>
+      {
+        analytics.umami.websiteId && <UmamiAnalytics {...analytics.umami} />
+      }
+      {
+        analytics.google.measurementId && <GoogleAnalytics {...analytics.google} />
+      }
+      {
+        analytics.microsoft.projectId && <MicrosoftClarity {...analytics.microsoft} />
+      }
     </>
-  )
-}
+  }
+
+  function UmamiAnalytics({
+    serverUrl,
+    websiteId,
+  }: {
+    serverUrl?: string,
+    websiteId: string,
+  }) {
+    const src = `${serverUrl || 'https://cloud.umami.is'}/script.js`
+
+    return <script defer src={src} data-website-id={websiteId} />
+  }
+
+
+  function GoogleAnalytics({
+    measurementId,
+  }: {
+    measurementId: string,
+  }) {
+    return (
+      <>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${measurementId}');`,
+        }}></script>
+      </>
+    )
+  }
+
+
+  function MicrosoftClarity({
+    projectId,
+  }: {
+    projectId: string,
+  }) {
+    return (
+      <>
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${projectId}");`
+        }}></script>
+      </>
+    )
+  }
